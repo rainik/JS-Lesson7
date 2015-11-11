073236a12b131e46180e4402d2b9818d33607d08
rainik <itrainik@gmail.com>
2015-11-11 20:12:26 +0200

    All from JS file was moved to HTML
    And added some variants how can I do these tasks.

--- myjs.js
+++ myjs.js
@@ -1,46 +1,52 @@
-﻿
-/*alert("My name is Sergey too!");
+﻿// All was commented and moved to the HTML file, except Task#2.
+// Task#2
+alert("My name is Myjs.js !");
 
+/*
+// Task #3
 var variabl1 = "Variable Any value";
-
 alert(variabl1);
+variabl1 = "New value!";
+alert("New value of this variable: "+variabl1);
 
+// Task4
 var a1 = "5";
 var a2 = "10";
 alert ("Original Values " + a1 +" "+ a2);
 a2 = a1;
 alert ("Changed values " + a1+ " " + a2);
 
+// Task #5
 var CONST1 = "RRRR";
 alert(CONST1);
-*/
-/*
-//task #6 1-6
-var arr1 = [];
-arr1.unshift(1);
-arr1.push("Random String");
-arr1.push(true);
-arr1.push(null);
-alert("There are "+ arr1.length +" into the array");
 
-//task #6 7
-alert("Elements of array: "+arr1[0] +" "+ arr1[1] +" " + arr1[2] +" " + arr1[3]);
+//task #6
+var arr1 = []; //# 1
+
+//For Jedies :)
+//arr1.unshift(1);
+//arr1.push("Random String");
+//arr1.push(true);
+//arr1.push(null);
+
+arr1 = [1, "Random String", true, "null"]; //# 2-5
+alert("There are "+ arr1.length +" elements into the array"); //# 6
+
+alert("Elements of array: "+(arr1)); //# 7
 
 
-//task #6 8-9
-var fifth_element = prompt("Input any value"," ");
+var fifth_element = prompt("Input any value"," ");//# 8
 arr1.push(fifth_element);
-alert("The fifth element is "+arr1[4]);
+alert("The fifth element is "+arr1[4]);// # 9
 
-*/
-/*
+
 //task #7 
 var new_arr = ["Джаз", "Блюз"]; //#1
 new_arr.push("Рок-н-Ролл"); // #2
 
 alert(new_arr);
 
-new_arr[new_arr.length-2]="Классика"; //3
+new_arr[new_arr.length-2]="Классика"; //#3
 alert(new_arr);
 
 alert("This element was deleted: "+new_arr.shift()); // #4
@@ -50,7 +56,7 @@ alert("Now we're adding two elements to the array");// #5
 new_arr.unshift("Регги");
 new_arr.unshift("Рэп");
 alert("Currently the array is looking like that: "+new_arr);
-*/
+
 
 // Task #8
 var yourName=prompt("What is your name?", "");
@@ -59,3 +65,4 @@ alert("Your name is "+yourName);
 //Task #9
 var question=confirm("Are you sure?");
 alert(question);
+*/
