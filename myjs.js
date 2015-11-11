d870a0932c89a5f25e28c753f91e170da3a7ea45
Sergey <itrainik@gmail.com>
2015-11-11 18:01:48 +0200

    Added all tasks
    Done all tasks from lesson 7

---
+++ myjs.js
@@ -0,0 +1,61 @@
+﻿
+/*alert("My name is Sergey too!");
+
+var variabl1 = "Variable Any value";
+
+alert(variabl1);
+
+var a1 = "5";
+var a2 = "10";
+alert ("Original Values " + a1 +" "+ a2);
+a2 = a1;
+alert ("Changed values " + a1+ " " + a2);
+
+var CONST1 = "RRRR";
+alert(CONST1);
+*/
+/*
+//task #6 1-6
+var arr1 = [];
+arr1.unshift(1);
+arr1.push("Random String");
+arr1.push(true);
+arr1.push(null);
+alert("There are "+ arr1.length +" into the array");
+
+//task #6 7
+alert("Elements of array: "+arr1[0] +" "+ arr1[1] +" " + arr1[2] +" " + arr1[3]);
+
+
+//task #6 8-9
+var fifth_element = prompt("Input any value"," ");
+arr1.push(fifth_element);
+alert("The fifth element is "+arr1[4]);
+
+*/
+/*
+//task #7 
+var new_arr = ["Джаз", "Блюз"]; //#1
+new_arr.push("Рок-н-Ролл"); // #2
+
+alert(new_arr);
+
+new_arr[new_arr.length-2]="Классика"; //3
+alert(new_arr);
+
+alert("This element was deleted: "+new_arr.shift()); // #4
+alert("This elements are left: "+new_arr);
+
+alert("Now we're adding two elements to the array");// #5
+new_arr.unshift("Регги");
+new_arr.unshift("Рэп");
+alert("Currently the array is looking like that: "+new_arr);
+*/
+
+// Task #8
+var yourName=prompt("What is your name?", "");
+alert("Your name is "+yourName);
+
+//Task #9
+var question=confirm("Are you sure?");
+alert(question);
